d39468edbd341f1530edc245c7b55d8a8a974e35
Pinny Geliebter <pgibralter@gmail.com>
2021-06-17 13:52:42 -0400

    Adds /params route and controller

--- app/controllers/param_names_controller.rb
+++ app/controllers/param_names_controller.rb
@@ -1,2 +1,9 @@
 class ParamNamesController < ApplicationController
+  def name_is_a
+    input = params["name"]
+    if input.first.downcase == "a"
+      render json: { message: input }
+    else
+    end
+  end
 end
