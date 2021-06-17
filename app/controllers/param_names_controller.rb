34878af75995e75f44942458ce5974f243196a96
Pinny Geliebter <pgibralter@gmail.com>
2021-06-17 13:58:29 -0400

    finishes /params route

--- app/controllers/param_names_controller.rb
+++ app/controllers/param_names_controller.rb
@@ -1,9 +1,10 @@
 class ParamNamesController < ApplicationController
   def name_is_a
     input = params["name"]
-    if input.first.downcase == "a"
-      render json: { message: input }
+    if !!input && input.first.downcase == "a"
+      render json: { name: input, message: "Hey, your name starts with the first letter of the alphabet!" }
     else
+      render json: { name: input, message: "Your name does not start with A" }
     end
   end
 end
