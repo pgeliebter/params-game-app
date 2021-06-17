32e31cccfb8bd873e11cf5dea8e081950cc4db9a
Pinny Geliebter <pgibralter@gmail.com>
2021-06-17 14:48:53 -0400

    adds some params games

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/params" => "param_names#name_is_a"
   get "/guess_game" => "param_names#game"
+  get "/segment_game/:num/" => "param_names#segment_game"
 end
